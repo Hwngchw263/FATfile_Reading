61596d0f5d35124203999c78360322b8decb12f7
hungcx263 <chuxuanhung2632001@gmail.com>
2024-10-29 17:11:36 +0700

    Update display data

--- Library/src/DisplayData.c
+++ Library/src/DisplayData.c
@@ -2,7 +2,21 @@
 
 void Display_Description(BootSector_Type *bootsector) {
     int i;
-    for (i=0;i< sizeof((*bootsector).filesystemType);i++){
-        printf("%c",(*bootsector).filesystemType[i]);
+    printf("Optional manufacturer description: ");
+    for (i=0;i< sizeof(bootsector->oemName);i++){
+        printf("%c",bootsector->oemName[i]);
     }
+    printf("\nNumber of File Allocation Tables: %d",bootsector->numFATs);
+    printf("\nMax directory entry: %d", bootsector->maxRootEntries);
+    printf("\nVolume Serial Number: %x",bootsector->volumeID);
+    printf("\nVolume Label: ");
+    for (i=0;i< sizeof(bootsector->volumeLabel);i++){
+        printf("%c",bootsector->volumeLabel[i]);
+    }
+    printf("\nFile system identifier: ");
+    for (i=0;i< sizeof(bootsector->filesystemType);i++){
+        printf("%c",bootsector->filesystemType[i]);
+    }
+     printf("\nSign: %x", bootsector->bootSectorSignature);
+    
 }
