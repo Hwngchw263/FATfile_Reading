a9547210a74e0ad911412faf6e8ae6f7bd2423d9
hungcx263 <chuxuanhung2632001@gmail.com>
2024-10-31 16:49:29 +0700

    Update display function

--- Library/inc/DisplayData.h
+++ Library/inc/DisplayData.h
@@ -10,6 +10,6 @@ typedef enum
 	HANDLE_FAT_SUCCESS=0U,
 	HANDLE_FAT_INVALID=1U
 }DATA_State;
-void Display_Description(BootSector_Type *bootsector);
+void Display_BootBlock(BootSector_Type *bootsector);
 void Display_Data(uint32_t cluster);
 #endif
