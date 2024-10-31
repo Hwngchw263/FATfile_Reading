6e0968491d3708c390bcbf99cb94a2a7b2751e31
hungcx263 <chuxuanhung2632001@gmail.com>
2024-10-31 16:53:12 +0700

    Update read root directory function

--- Library/src/UserHandling.c
+++ Library/src/UserHandling.c
@@ -1,11 +1,13 @@
 #include "UserHandling.h"
 
 BootSector_Type bootsector;
-
+DirectoryType rootdirectory;
 void Open(const char* file){
+
     Open_FAT_File(file);
     Read_Boot_blockArea(&bootsector);
-    Display_Description(&bootsector);
+    Display_BootBlock(&bootsector);
+    Read_Root_DirectoryArea(&bootsector, &rootdirectory);
 }
 
 void Close(){
