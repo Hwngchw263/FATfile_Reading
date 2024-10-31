cd1ecb98b014eafc72871dd4799b9e2642784794
hungcx263 <chuxuanhung2632001@gmail.com>
2024-10-31 16:52:13 +0700

    Update read root directory function

--- Library/src/FileHandling.c
+++ Library/src/FileHandling.c
@@ -36,16 +36,58 @@ ProcessStatus_t Read_Boot_blockArea(BootSector_Type *bootsector) {
 	}
 }
 
-ProcessStatus_t Read_Root_DirectoryArea(BootSector_Type *bootsector, DirectoryEntry_Type *rootdirectory) {
+ProcessStatus_t Read_Root_DirectoryArea(const BootSector_Type *bootsector, DirectoryType *rootdirectory) {
     ProcessStatus_t status = PROCESS_SUCCESS;
     if (fileptr == NULL) {
 		status = PROCESS_FAILURE;
 	}
 	else {
 		uint32_t rootdir = (bootsector->sectorsPerFAT * bootsector->numFATs + bootsector->reservedSectors) * bootsector->bytesPerSector;
+		rootdirectory->Entry_list = malloc(sizeof(SFNEntry)* bootsector->numRootEntries);
+		rootdirectory->number_current_entries = 0;
         printf("\nRoot directory position: 0x%x", rootdir);
-	}	
-}
-ProcessStatus_t Read_Entry() {
+		printf("\nSize of an SFN entry: %d", sizeof(SFNEntry));
+		printf("\nSize of an LFN entry: %d", sizeof(LFNEntry));
+		/*move current position to root area offset*/
+		fseek(fileptr,rootdir,SEEK_SET);
+		// printf("\nLog: 0x%x",fgetc(fileptr));
+		uint8_t i, j;
+		SFNEntry Temp_SFN_entry;
+		LFNEntry Temp_LFN_entry;
+		uint8_t File_Attribute;
+		if (rootdirectory->Entry_list != NULL ) {
+			for (i = 0; i < bootsector->numRootEntries; i++) {
+				/*move to attribute field position*/
+				fseek(fileptr,0x0B,SEEK_CUR);
+				//Read attribute field
+				File_Attribute = fgetc(fileptr);
+				printf("\nLog attribute: 0x%x", File_Attribute);
+				/*Return to start position of entry*/
+				fseek(fileptr,-0x0C,SEEK_CUR);
+				if (File_Attribute != 0x0F) {
+					fread(&Temp_SFN_entry,sizeof(SFNEntry), 1, fileptr);
+					if(Temp_SFN_entry.filename[0] != 0x0) {
+						*(rootdirectory->Entry_list + rootdirectory->number_current_entries) = Temp_SFN_entry;
+						// *(rootdirectory+i) = Temp_SFN_entry;
+						rootdirectory->number_current_entries++;
+					}
+					else {
+						break;
+					}
+				}
+				else {
+					fread(&Temp_LFN_entry,sizeof(LFNEntry), 1, fileptr);
+				}
 
+			}
+		}
+		printf("\nNumber entry: %d",rootdirectory->number_current_entries);
+		printf("\nAll entry: ");
+		for (i = 0; i< rootdirectory->number_current_entries; i++){
+			printf("\nEntry %d : ", i+1);
+			for (j=0; j < sizeof(rootdirectory->Entry_list[i].filename) ; j++) {
+				printf("%c",rootdirectory->Entry_list[i].filename[j]);
+			}
+		}
+	}	
 }
