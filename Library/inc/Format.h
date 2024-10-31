411c2a19030044a1ec3c964de20f4db2607333a9
hungcx263 <chuxuanhung2632001@gmail.com>
2024-10-31 16:51:24 +0700

    Update directory format

--- Library/inc/Format.h
+++ Library/inc/Format.h
@@ -12,7 +12,7 @@ typedef struct{
     uint8_t     sectorsPerCluster;        // Offset 0x0D: Number of blocks per allocation unit
     uint16_t    reservedSectors;          // Offset 0x0E: Number of reserved sectors, typically 1
     uint8_t     numFATs;                  // Offset 0x10: Number of File Allocation Tables
-    uint16_t    maxRootEntries;           // Offset 0x11: Number of root directory entries
+    uint16_t    numRootEntries;           // Offset 0x11: Number of root directory entries
     uint16_t    totalSectors16;           // Offset 0x13: Total number of sectors (if < 65536; else 0)
     uint8_t     mediaDescriptor;          // Offset 0x15: Media descriptor byte
     uint16_t    sectorsPerFAT;            // Offset 0x16: Number of sectors per FAT
@@ -56,6 +56,45 @@ typedef struct {
     uint16_t date;            // 0x18 - Date created or last updated (2 bytes)
     uint16_t startCluster;    // 0x1a - Starting cluster number for file (2 bytes)
     uint32_t fileSize;        // 0x1c - File size in bytes (4 bytes)
-} DirectoryEntry_Type;
+} SFNEntry;
+
+typedef struct {
+    uint32_t number_current_entries;
+    SFNEntry *Entry_list;
+}DirectoryType;
+
+typedef struct {
+    uint8_t order;              // Order of the LFN entry (1 byte)
+    uint16_t name_part1[5];     // Part 1 of the name (5 Unicode characters)
+    uint8_t attribute;          // Attribute, always 0x0F for LFN
+    uint8_t type;               // Type, always 0x00 for LFN
+    uint8_t checksum;           // Checksum of the corresponding short file name (8.3)
+    uint16_t name_part2[6];     // Part 2 of the name (6 Unicode characters)
+    uint16_t cluster;           // Cluster, always 0x0000
+    uint16_t name_part3[2];     // Part 3 of the name (2 Unicode characters)
+} LFNEntry;
+
+
+typedef struct {
+    uint8_t Hours : 5;
+    uint8_t Minutes : 6;
+    uint8_t Two_second_periods :5;
+}Time_Raw_type;
+
+typedef struct {
+    uint8_t Year : 7;
+    uint8_t Month : 4;
+    uint8_t Day :5;
+}Date_Raw_type;
+
+typedef union {
+    uint16_t Time;
+    Time_Raw_type Raw_Time;
+}Time_Type;
+
+typedef union {
+    uint16_t Date;
+    Date_Raw_type Raw_Date;
+}Date_Type;
 
 #endif
